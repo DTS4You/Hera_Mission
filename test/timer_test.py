01f578c9270548a150bea63cd17ecf354521f193
schwarzn <n.schwarz@wksgruppe.de>
2024-05-14 14:08:41 +0200

    Init

---
+++ test/timer_test.py
@@ -0,0 +1,40 @@
+# YD-RP2040 Board
+# User Button (GPIO24)
+# User LED    (GPIO25)
+
+from machine import Pin, Timer
+ 
+led = Pin(25, Pin.OUT)
+Counter = 0
+Fun_Num = 0
+ 
+def fun(tim):
+    global Counter
+    Counter = Counter + 1
+    print(Counter)
+    led.value(Counter%2)
+ 
+
+def main():
+
+    tim = Timer(-1)
+    tim.init(period=1000, mode=Timer.PERIODIC, callback=fun)
+
+    print("=== End of Main ===")
+
+# ==============================================================================
+# ==============================================================================
+    
+# ###############################################################################
+# ### Main                                                                    ###
+# ###############################################################################
+
+
+if __name__ == "__main__":
+    
+    main()      # Start Main $$$
+
+# Normal sollte das Programm hier nie ankommen !
+print("___End of Programm___ !!!")
+
+# ##############################################################################
