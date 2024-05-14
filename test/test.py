6a942fcbbe0968271e02ade6e65d7ac3d9d9b686
schwarzn <n.schwarz@wksgruppe.de>
2024-05-14 14:43:03 +0200

    Init

--- test/test.py
+++ test/test.py
@@ -1,14 +1,46 @@
 ###############################################################################
 ### Test Programm                                                           ###
 ###############################################################################
+import time
 
 pix_array = [[ 1, 1, 1, 1],
-             [ 0, 1, 2, 3]]
+             [ 0, 2, 4, 8]]
 
 
-for i in range(len(pix_array[0])):
-    print(pix_array[1][i])
+
+# #############################################################################
+# ### Main                                                                  ###
+# #############################################################################
+
+def main():
+    for i in range(len(pix_array[0])):
+        print(pix_array[1][i])
 
 
+    try:
+        print("Start")
+        for i in range(10):
+            time.sleep(1)
+            print("Schleife")
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt")
+        i = 0
+    finally:
+        print("Exiting the program")
+
+
+
+# #############################################################################
+
+if __name__ == "__main__":
+    
+    print("___ Start of Programm ___ !!!")
+    
+    main()      # Start Main $$$
+
+# Normal sollte das Programm hier nie ankommen !
+print("___ End of Programm ___ !!!")
+
+# #############################################################################
 
 
